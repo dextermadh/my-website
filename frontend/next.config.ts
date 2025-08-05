de263c3f42983b145d02b5def128a441054e6291
dextermadh <93965850+dextermadh@users.noreply.github.com>
2025-08-05 13:41:05 +0530

    v1.1

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -3,6 +3,11 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: "export",
+    images: {
+    unoptimized: true, // GitHub Pages does not support Image Optimization
+  },
+  basePath: '/my-website', // Replace with your repo name
+  assetPrefix: '/my-website/', // Same as basePath
 };
 
 export default nextConfig;
